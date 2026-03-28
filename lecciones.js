f9cab9813e5517f9fd07c5cc3e7172314995d4f6
Tsunki <202082876+Tsunkichi@users.noreply.github.com>
2026-03-28 01:24:40 -0600

    Fix paths and folder names in LESSONS_DATA
    cambiado a minusculas

--- lecciones.js
+++ lecciones.js
@@ -1,14 +1,14 @@
 window.LESSONS_DATA = [
   {
     "title": "Chai winiajai: cómo llegar y saludar",
-    "path": "lecciones/chai-winiajai-como-llegar-y-saludar-raw-dAS0KtEX/content/index.html",
-    "folderName": "chai-winiajai-como-llegar-y-saludar-raw-dAS0KtEX",
-    "image": "lecciones/chai-winiajai-como-llegar-y-saludar-raw-dAS0KtEX/content/assets/UagRFPMVXFUbM0Hl.jpg"
+    "path": "lecciones/chai-winiajai-como-llegar-y-saludar-raw-das0ktex/content/index.html",
+    "folderName": "chai-winiajai-como-llegar-y-saludar-raw-das0ktex",
+    "image": "lecciones/chai-winiajai-como-llegar-y-saludar-raw-das0ktex/content/assets/uagrfpmvxfubm0hl.jpg"
   },
   {
     "title": "Preséntate y pregunta nombres en shuar",
-    "path": "lecciones/presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-JU8Y45Jk/content/index.html",
-    "folderName": "presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-JU8Y45Jk",
-    "image": "lecciones/presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-JU8Y45Jk/content/assets/uspRS-ch4FS3AVAG.jpg"
+    "path": "lecciones/presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-ju8y45jk/content/index.html",
+    "folderName": "presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-ju8y45jk",
+    "image": "lecciones/presentate-y-pregunta-nombres-en-shuar-microaprendizaje-para-viajeros-raw-ju8y45jk/content/assets/usprs-ch4fs3avag.jpg"
   }
 ];
